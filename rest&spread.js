c27d24eee4c4528669f233037aad67ea1ea38294
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-29 17:50:33 +0500

    sprea&rest&Destructure

--- rest&spread.js
+++ rest&spread.js
@@ -3,3 +3,25 @@ let b = [4, 5 ,6];
 let c = a.concat(b);
 console.log(c);
 console.log(...a, ...b);
+
+const d = [1, 2, 3, 4];
+const [first, ...third] = d;
+console.log([first,...third]);
+
+
+const e = {
+    name: "ali",
+    value: "Nill",
+    class: "one"
+}
+const {previous, ...newd} = e;
+console.log(previous, newd);
+
+const obj1 = [{a:1, b:2, c:3}];
+const obj2 = [{d:4, e:5, f:6}];
+console.log(...obj1, ...obj2);
+
+
+const g = ["ali", "ahmed", "aliya"];
+const [,,girl]= g;
+console.log(girl);
